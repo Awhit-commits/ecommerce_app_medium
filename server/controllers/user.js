c5a4133178a6c21d1598ae55389accbeb373bbbb
Awhit-commits <ndrw_whtmr@ymail.com>
2020-09-06 22:00:23 -0500

    Added login route with authenication/jwt

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,9 @@
+//Importing dependencies
 const User = require("../models/user");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+const user = require("../models/user");
 //sanity message
 exports.signUp = (req, res) => {
   console.log("req.body", req.body);
@@ -11,4 +15,24 @@ exports.signUp = (req, res) => {
     user.hashed_password = undefined;
     res.json({ user });
   });
+};
+
+exports.logIn = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: "User with that email is not found. Please signup!" });
+    }
+    if(!user.authenticate(password)){
+      return res.status(401).json({error:"Email and/or password does not match"})
+    }
+    //if user is found, make sure email and password match
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+  
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUp } = require("../controllers/user");
+const { signUp,logIn } = require("../controllers/user");
+
 const {userSignupValidator} = require("../validator/index")
+
 
 //Signup route
 router.post("/signup",userSignupValidator, signUp);
+
+//Login route 
+router.post("/login",logIn);
 
 module.exports = router;
